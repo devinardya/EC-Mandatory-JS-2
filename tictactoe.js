144c07a94016b459b1971ea8dddd538f679970de
Devina <devinardya@gmail.com>
2019-11-13 12:45:07 +0100

    fixing scoreboard and game logic

--- tictactoe.js
+++ tictactoe.js
@@ -1,50 +1,147 @@
 const block = document.querySelectorAll(".block");
 const startAgain = document.querySelector("#startAgain");
+const reset = document.querySelector("#resetAgain");
+const humanPlayers = document.querySelector("#playwithhuman");
+const withComp = document.querySelector("#playwithcomp");
+let gamestartAlert = document.querySelector(".alert");
+let gamestartAlertText = document.querySelector(".gamestart");
+let winningAlertText = document.querySelector(".winning");
+let spanP1Score = document.querySelector(".p1");
+let spanP2Score = document.querySelector(".p2");
+
+let scorep1 = 0;
+let scorep2 = 0;
+spanP1Score.textContent = scorep1;
+spanP2Score.textContent = scorep2;
 let counter = 0;
 let x = "X"; // even (first player)
 let o = "O"; // odd (second player)
 
 let gameOver = false;
 
+//choosing between playing 2 human players or versus computer
+humanPlayers.addEventListener("click", startGameHuman);
+withComp.addEventListener("click", startGameComp);
+
+
 
 
 // to start the game again
-startAgain.addEventListener("click", function(){
-    for (let each of block){
-        each.innerHTML="";
-    }
-    counter = 0;
-    gameOver = false;
-});
+startAgain.addEventListener("click", restartGame);
 
-// initiating the game
-for (let i = 0; i < block.length; i++){
-    eachblock = block[i];
-
-    // add event listeners
-    if (counter >= 0 || counter <= 8 ){
-        eachblock.addEventListener("click", function(){
-
-            let a = document.createElement("a");
-            console.log(counter);
-            // if statements to see if the block is empty or not
-
-            if (block[i].textContent === "" && gameOver == false){
-                if (isEven(counter)){
-                    a.textContent = x;
-                    block[i].appendChild(a);
-                } else {
-                    a.textContent = o;
-                    block[i].appendChild(a);
-                }
-                counter++;          // add one to the counter
-                currentState();     // check the current game state
-            }
-            
-        });
-    }
+function restartGame(){
+        gamestartAlert.style.display = "none";
+        winningAlertText.style.display = "none";
+        for (let each of block){
+            each.innerHTML="";
+        }
+        counter = 0;
+        gameOver = false;
     
 };
+
+// to reset the whole game 
+reset.addEventListener("click", resetGame);
+
+function resetGame(){
+    location.reload();
+}
+
+
+// when the game have 2 players
+function startGameHuman(){
+    
+    restartGame();
+
+    gamestartAlert.style.display = "block";
+    gamestartAlertText.style.display = "block";
+   
+
+    setTimeout(function(){
+        gamestartAlert.style.display = "none";
+        gamestartAlertText.style.display = "none";
+    }, 1000);
+
+    // initiating the game
+    for (let i = 0; i < block.length; i++){
+        eachblock = block[i];
+
+        // add event listeners
+        if (counter >= 0 || counter <= 8 ){
+            eachblock.addEventListener("click", function(){
+
+                let a = document.createElement("a");
+
+                // if statements to see if the block is empty or not
+                if (block[i].textContent === "" && gameOver === false){
+                    if (isEven(counter)){
+                        a.textContent = x;
+                        block[i].appendChild(a);
+                    } else {
+                        a.textContent = o;
+                        block[i].appendChild(a);
+                    }
+                    counter++;          // add one to the counter
+                    currentState();     // check the current game state
+                };
+                
+            });
+        };
+        
+    };
+};
+
+
+
+// when the game have 2 players
+function startGameComp(){
+    restartGame();
+
+    gamestartAlert.style.display = "block";
+    gamestartAlertText.style.display = "block";
+   
+
+    setTimeout(function(){
+        gamestartAlert.style.display = "none";
+        gamestartAlertText.style.display = "none";
+    }, 1000);
+
+    // initiating the game
+    for (let i = 0; i < block.length; i++){
+        eachblock = block[i];
+        
+        // add event listeners
+        if (counter >= 0 || counter <= 8 ){
+            eachblock.addEventListener("click", function(){
+                
+                let a = document.createElement("a");
+                a.style.cursor = "pointer";
+                // if statements to see if the block is empty or not
+                if (block[i].textContent === "" && gameOver === false){
+                    if (isEven(counter)){
+                        a.textContent = x;
+                        block[i].appendChild(a);
+                    } else {
+                        eachblock.addEventListener("mouseup", function(){
+                            setTimeout(function(){
+                                i = Math.floor(Math.random() * 10);
+                                a.textContent = o;
+                                block[i].appendChild(a);
+                            }, 100);
+                        })
+                        
+                    }
+                    counter++;          // add one to the counter
+                    currentState();     // check the current game state
+                };
+                
+            });
+        };
+        
+    };
+};
+
+
 
 // true or false if "n" is even
 function isEven(n) {
@@ -64,9 +161,7 @@ function currentState(){
         } else if (input === o){
             arrayO.push(block[j].id);
         }    
-    }
-    console.log("arrx: " + arrayX);
-    console.log("arr0: " + arrayO);
+    };
 
     checkWinner(arrayX, arrayO)
 };
@@ -82,25 +177,53 @@ function checkWinner(input1, input2){
     for (let k = 0; k < winningComb.length; k++){
         let numArr = winningComb[k].split("");
 
-        // console.log(numArr);
         let num1 = numArr[0];   // first winning number
         let num2 = numArr[1];   // second winning number
         let num3 = numArr[2];   // third winning number
-         
-        //console.log(arrX.indexOf(num1));
 
-        // check if x is winner
+        // check if Player One is the winner
         if (arrX.indexOf(num1) !== -1 && arrX.indexOf(num2) !== -1 && arrX.indexOf(num3) !== -1 ){
-            console.log("WINNNER CHICKEN DINNER!!")
+            winningCelebration("arrX")
             gameOver = true;
-        }
-
+        } 
+        // check if Player Two is the winner
         if (arrO.indexOf(num1) !== -1 && arrO.indexOf(num2) !== -1 && arrO.indexOf(num3) !== -1 ){
-            console.log("O IS THE BEST!!!!!")
+            winningCelebration("arrO")
+            gameOver = true;
+        } 
+
+        if (counter === 9){
+            winningCelebration("draw")
             gameOver = true;
         }
+    };
 
+};
+
+function winningCelebration(winner){
+    if (winner === "arrX"){
+        setTimeout(function(){
+            winningAlertText.textContent = "PLAYER 1 WINS";
+            gamestartAlert.style.display = "block";
+            winningAlertText.style.display = "block";
+        }, 200);
+        scorep1++;
+        spanP1Score.textContent = scorep1;
+    } else if (winner === "arrO"){
+        setTimeout(function(){
+            winningAlertText.textContent = "PLAYER 2 WINS";
+            gamestartAlert.style.display = "block";
+            winningAlertText.style.display = "block";
+        }, 200); 
+        scorep2++;
+        spanP2Score.textContent = scorep2;  
+    }else if (winner === "draw"){
+        setTimeout(function(){
+            winningAlertText.textContent = "GAME DRAW";
+            gamestartAlert.style.display = "block";
+            winningAlertText.style.display = "block";
+            winningAlertText.style.transform = "translate(-25%, -50%)";
+        }, 200); 
     }
-
 };
 
